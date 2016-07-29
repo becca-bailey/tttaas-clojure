830dab6f2d84ba97ed317915e4ef1429d989b9fb
Becca Nelson <beccanelson88@gmail.com>
2016-07-28 21:45:06 -0400

    Add functions to convert board and board state to call minimax

--- spec/tic_tac_toe/iOS_functions_spec.clj
+++ spec/tic_tac_toe/iOS_functions_spec.clj
@@ -5,4 +5,17 @@
 (describe "iOS functions"
   (context "#minimax-move"
     (it "returns the best computer move given a board"
-      (should= 8 (iOS/minimax-move [0,1,2,3,4,5,6,7,8])))))
+      (should= 8 (iOS/minimax-move [0,1,2,3,4,5,6,7,8]))))
+
+    (context "#minimax-move"
+      (it "returns the best computer move given a board"
+        (should= 8 (iOS/minimax-move ["" "" "" "" "" "" "" "" ""]))))
+
+    (context "#return-move-or-index"
+      (it "returns the index if the move is empty"
+        (should= 0 (iOS/return-move-or-index [0 ""]))))
+
+    (context "#spot-with-index"
+      (it "returns a collection of vectors with the spot and the index"
+        (should= [[0 ""][1 ""][2 ""][3 ""][4 ""][5 ""][6 ""][7 ""][8 ""]]
+        (iOS/spot-with-index ["" "" "" "" "" "" "" "" ""])))))
--- src/tic_tac_toe/iOS_functions.clj
+++ src/tic_tac_toe/iOS_functions.clj
@@ -4,3 +4,18 @@
 
 (defn minimax-move [board]
   (ai/best-computer-move (game/game-state board)))
+
+(defn spot-with-index [board]
+  (map-indexed vector board))
+
+(defn return-move-or-index [move-and-index]
+  (let [[index move] move-and-index]
+    (if (= move "") index move)))
+
+(defn convert-board [board]
+  (let [indexed-board (spot-with-index board)]
+    (map return-move-or-index indexed-board)))
+
+(defn minimax-move [board-from-java]
+  (let [game-state (game/game-state (convert-board board-from-java))]
+    (ai/best-computer-move game-state)))
