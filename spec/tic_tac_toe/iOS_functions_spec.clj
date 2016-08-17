bcc6f47de77704f8f242f52cd86300c935d5ae23
Becca Nelson <beccanelson88@gmail.com>
2016-08-17 16:04:17 -0500

    Add minimax-move function for computer-vs-computer

--- spec/tic_tac_toe/iOS_functions_spec.clj
+++ spec/tic_tac_toe/iOS_functions_spec.clj
@@ -11,6 +11,12 @@
       (it "returns the best computer move given a board"
         (should= 8 (iOS/minimax-move ["" "" "" "" "" "" "" "" ""]))))
 
+      (it "return the best computer move given a board and a marker"
+        (should= 8 (iOS/minimax-move ["" "" "" "" "" "" "" "" ""] "O")))
+
+      (it "return the best computer move given a board and a marker"
+        (should= 4 (iOS/minimax-move ["" "" "" "" "" "" "" "" "O"] "X")))
+
     (context "#return-move-or-index"
       (it "returns the index if the move is empty"
         (should= 0 (iOS/return-move-or-index [0 ""]))))
@@ -18,4 +24,8 @@
     (context "#spot-with-index"
       (it "returns a collection of vectors with the spot and the index"
         (should= [[0 ""][1 ""][2 ""][3 ""][4 ""][5 ""][6 ""][7 ""][8 ""]]
-        (iOS/spot-with-index ["" "" "" "" "" "" "" "" ""])))))
+        (iOS/spot-with-index ["" "" "" "" "" "" "" "" ""]))))
+
+    (context "#switch-marker"
+      (it "returns the opposite marker"
+        (should= "X" (iOS/switch-marker "O")))))
--- src/tic_tac_toe/iOS_functions.clj
+++ src/tic_tac_toe/iOS_functions.clj
@@ -1,9 +1,15 @@
 (ns tic-tac-toe.iOS-functions
   (:require [tic-tac-toe.ai :as ai]
-            [tic-tac-toe.game :as game]))
+            [tic-tac-toe.game :as game]
+            [tic-tac-toe.player :as player]))
 
-(defn minimax-move [board]
-  (ai/best-computer-move (game/game-state board)))
+(defn switch-marker [marker]
+  (if (= marker "X") "O" "X"))
+
+(defn get-game-state [board computer-marker]
+  (let [computer-player (player/computer computer-marker)
+        human-player (player/human (switch-marker computer-marker))]
+    (game/game-state board [human-player computer-player])))
 
 (defn spot-with-index [board]
   (map-indexed vector board))
@@ -16,6 +22,10 @@
   (let [indexed-board (spot-with-index board)]
     (into [] (map return-move-or-index indexed-board))))
 
-(defn minimax-move [board-from-java]
-  (let [game-state (game/game-state (convert-board board-from-java))]
-    (ai/best-computer-move game-state)))
+(defn minimax-move
+  ([board-from-java]
+    (let [game-state (game/game-state (convert-board board-from-java))]
+      (ai/best-computer-move game-state)))
+  ([board-from-java computer-marker]
+    (let [game-state (get-game-state (convert-board board-from-java) computer-marker)]
+      (ai/best-computer-move game-state))))
